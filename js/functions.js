294c9c205bc0244d1eac290f8eff7458a04c1e88
BasiaM1 <b.murzynowska@wp.pl>
2020-06-05 17:05:22 +0200

    Add changes in css and results

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,7 @@ function printMessage(msg) {
     let div = document.createElement('div');
     div.innerHTML = msg;
     document.getElementById('messages').appendChild(div);
+    document.querySelector('#messages div:first-child').classList.add('bold');
 }
 
 function clearMessages() {
--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@
 
     const getMoveName = function (argMoveId) {
       if (argMoveId == 1) {
-        return "kamień";
+        return "rock";
       }
       if (argMoveId == 2) {
-        return "papier";
+        return "paper";
       }
       if (argMoveId == 3) {
-        return "nożyce";
+        return "scissors";
       }
 
       printMessage("Nie znam ruchu o id " + argMoveId + ".");
@@ -22,34 +22,39 @@
     console.log("Wylosowana liczba to: " + randomNumber);
 
     const argComputerMove = getMoveName(randomNumber);
-
-    printMessage("Ruch komputera to: " + argComputerMove);
+    printMessage("Game result")
+    printMessage("Computer move: " + argComputerMove);
 
     console.log("Gracz wpisał: " + playerInput);
 
     const argPlayerMove = getMoveName(playerInput);
 
-    printMessage("Twój ruch to: " + argPlayerMove);
+    printMessage("Your move: " + argPlayerMove);
 
     const displayResult = function (argComputerMove, argPlayerMove) {
       console.log("moves:", argComputerMove, argPlayerMove);
 
       if (
-        (argComputerMove == "kamień" && argPlayerMove == "papier") ||
-        (argComputerMove == "nożyce" && argPlayerMove == "kamień") ||
-        (argComputerMove == "papier" && argPlayerMove == "nożyce")
+        (argComputerMove == "rock" && argPlayerMove == "paper") ||
+        (argComputerMove == "scissors" && argPlayerMove == "rock") ||
+        (argComputerMove == "paper" && argPlayerMove == "scissors")
       ) {
-        printMessage("Ty wygrywasz!");
+        wins.innerHTML = ++gameSummary.wins;
+        printMessage("You win!");
       } else if (argComputerMove == argPlayerMove) {
-        printMessage("Remis!");
+        draws.innerHTML = ++gameSummary.draws;
+        printMessage("Draw!");
       } else if (argPlayerMove == "nieznany ruch") {
         printMessage("Wybrałeś inną liczbę niż 1, 2 lub 3. Spróbuj ponownie!");
       } else {
-        printMessage("Przegrałeś :-(");
+        losses.innerHTML = ++gameSummary.losses;
+        printMessage("You lose :-(");
       }
     }
 
     displayResult(argComputerMove, argPlayerMove);
+
+    gameNum.innerHTML = ++gameSummary.numbers;
   }
   document.getElementById('play-rock').addEventListener('click', function () {
     playGame(1);
@@ -60,4 +65,26 @@
   document.getElementById('play-scissors').addEventListener('click', function () {
     playGame(3);
   });
+
+  const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+  }
+
+  const wins = document.querySelector('p span.wins');
+  const draws = document.querySelector('p.draws span')
+  const losses = document.querySelector('p span.losses')
+  const gameNum = document.querySelector('.panel-result .numbers span')
+
+  const endGame = function () {
+    wins.innerHTML = 0;
+    draws.innerHTML = 0;
+    losses.innerHTML = 0;
+    gameNum.innerHTML = 0;
+    clearMessages();
+
+  }
+  document.getElementById('end').addEventListener('click', endGame);
 }
